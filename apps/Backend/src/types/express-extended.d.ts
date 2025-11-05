ef0398ada2cb082742252ee950cc6cf13f05f5dd
Daif <applediegoaif@gmail.com>
2025-11-05 02:39:19 -0600

    @jcv/api - fix (express-types): Payload params object -> record.

--- apps/Backend/src/types/express-extended.d.ts
+++ apps/Backend/src/types/express-extended.d.ts
@@ -3,13 +3,13 @@ declare namespace Express {
         // Basado en respuestas HTTP
         
         // 2XX
-        sendSuccess(payload: object, msg?: string, pagination?: Record<string, any>): void;                  // 200
-        sendCreated(payload: object, msg?: string): void;                                                    // 201
-        sendAccepted(payload: object, msg?: string): void;                                                   // 202
-        sendNoContent(msg?: object): void;                                                                   // 204
+        sendSuccess(payload: Record<string, any>, msg?: string, pagination?: Record<string, any>): void;                  // 200
+        sendCreated(payload: Record<string, any>, msg?: string): void;                                                    // 201
+        sendAccepted(payload: Record<string, any>, msg?: string): void;                                                   // 202
+        sendNoContent(msg?: Record<string, any>): void;                                                                   // 204
 
         // 4XX
-        sendBadRequest(reason?: string, fields?: string | object | Record<string, string>): void;            // 400
+        sendBadRequest(reason?: string, fields?: string | Record<string, any>): void;            // 400
         sendUnauthorized(reason?: string): void;                                                             // 401  ===> The server DOES NOT know who the user is, so they would have to login.
         sendForbidden(reason?: string): void;                                                                // 403  ===> The server DOES know who the user is, BUT they DO NOT have permission to access the resource.
         sendNotFound(reason?: string): void;                                                                 // 404
