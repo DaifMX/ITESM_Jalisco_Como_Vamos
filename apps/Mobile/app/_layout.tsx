2652e7167012701b92cc12851043958e53ee8f17
Daif <applediegoaif@gmail.com>
2025-11-25 00:36:03 -0600

    @jcv/app - feat(version checker): Added a component that wraps the whole application to check if the device in which the app is installed meets the minimum version requirements.

--- apps/Mobile/app/_layout.tsx
+++ apps/Mobile/app/_layout.tsx
@@ -12,26 +12,29 @@ import { useColorScheme } from "@/hooks/use-color-scheme";
 
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "@/global.css";
+import { PlatformVersionCheck } from "@/components/platform-version-checker";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <SafeAreaView className="flex-1 bg-white" edges={["top", "right", "left", "bottom"]}>
-      <GluestackUIProvider mode="light">
-        <ThemeProvider
-          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-        >
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="home" options={{ title: "Inicio" }} />
-            <Stack.Screen name="questions" options={{ title: "Preguntas" }} />
-            <Stack.Screen name="questionData" options={{ title: "Datos de pregunta" }} />
-            <Stack.Screen name="my-account" options={{ title: "Mi Cuenta" }} />
-            <Stack.Screen name="login" options={{ headerShown: false }} />
-            <Stack.Screen name="register" options={{ headerShown: false }} />.
-          </Stack>
-        </ThemeProvider>
-      </GluestackUIProvider>
-    </SafeAreaView>
+    <PlatformVersionCheck>
+      <SafeAreaView className="flex-1 bg-white" edges={["top", "right", "left", "bottom"]}>
+        <GluestackUIProvider mode="light">
+          <ThemeProvider
+            value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+          >
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="home" options={{ title: "Inicio" }} />
+              <Stack.Screen name="questions" options={{ title: "Preguntas" }} />
+              <Stack.Screen name="questionData" options={{ title: "Datos de pregunta" }} />
+              <Stack.Screen name="my-account" options={{ title: "Mi Cuenta" }} />
+              <Stack.Screen name="login" options={{ headerShown: false }} />
+              <Stack.Screen name="register" options={{ headerShown: false }} />.
+            </Stack>
+          </ThemeProvider>
+        </GluestackUIProvider>
+      </SafeAreaView>
+    </PlatformVersionCheck>
   );
 }
