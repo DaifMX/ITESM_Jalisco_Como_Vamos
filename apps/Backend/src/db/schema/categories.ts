f38104cb0442a554dfb3c1f2826b069c7d6c9382
Daif <applediegoaif@gmail.com>
2025-11-30 01:05:31 -0600

    @jcv/api: feat(categories): Categories now manage their color and icon from the database.

--- apps/Backend/src/db/schema/categories.ts
+++ apps/Backend/src/db/schema/categories.ts
@@ -6,6 +6,8 @@ import { questions } from '@/db/schema';
 export const categories = pgTable('categories', {
     id: uuid('id').primaryKey().default(sql`gen_random_uuid()`),
     name: text('name').unique(),
+    icon: text('icon'),
+    color: text('color'),
 });
 
 export const categoryRelations = relations(categories, ({ many }) => ({
